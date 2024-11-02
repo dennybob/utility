e9d0db40b4775efb602175d2182f065ac5a230c8
dennybob <dennis.voss79@gmail.com>
2024-11-02 11:19:24 -0700

    Finished createSimeCode.py   - posted simCode.nc   - changed simulator.nc to simulator_sample

--- dev/engrave macro/createSimCode.py
+++ dev/engrave macro/createSimCode.py
@@ -7,85 +7,52 @@ with open('/Users/dennybob/Documents/GitHub/utility/dev/engrave macro/engrave.nc
     for line in file:
         prog.append(line.strip())
 
+# Remove lines that start with '%', 'O', '(', '#', 'IF', 'GOTO', 'G17'
+prog = [x for x in prog if not x.startswith('%')] 
+prog = [x for x in prog if not x.startswith('%')]
+prog = [x for x in prog if not x.startswith('O')]
+prog = [x for x in prog if not x.startswith('(')]
+prog = [x for x in prog if not x.startswith('#')]
+prog = [x for x in prog if not x.startswith('IF')]
+prog = [x for x in prog if not x.startswith('GOTO')] 
+prog = [x for x in prog if not x.startswith('G17')]
 
+# Remove lines that start with 'N90' or 'N91' or 'N999'
+prog = [x for x in prog if not x.startswith('N90')]
+prog = [x for x in prog if not x.startswith('N91')]
+prog = [x for x in prog if not x.startswith('N999')]
 
-# Remove full lines that start with these characters
+# Remove lines that contain 'G68' or 'G69'
+prog = [x for x in prog if 'G68' not in x]
+prog = [x for x in prog if 'G69' not in x]
+
+# Remove lines that contain 'G50' or 'G51'
+prog = [x for x in prog if 'G50' not in x]
+prog = [x for x in prog if 'G51' not in x]
+
+# Remove lines that contain 'Z#28'
+prog = [x for x in prog if 'Z#28' not in x]
+
+# Remove lines tthat contain 'G#31'
+prog = [x for x in prog if 'G#31' not in x]
+
+# Remove empty lines
+prog = list(filter(None, prog))
+
+# Change '#26' to '.1'
+prog = [x.replace('#26', '.1') for x in prog]
+
+# Change '#8' to '.5'
+prog = [x.replace('#8', '.5') for x in prog]
+
+# Change '#9' to '1.'
+prog = [x.replace('#9', '1.') for x in prog]
+
+# Print program
 for line in prog:
-    if line.startswith('%'):
-        prog.pop(prog.index(line))
-for line in prog:
-    if line.startswith('O'):
-        prog.pop(prog.index(line))
-for line in prog:
-    if line.startswith('GOTO'):
-        prog.pop(prog.index(line))
-for line in prog:                       # FOR SOME REASON IT ONLY POPS THE FIRST INSTANCE, ALL THE OTHERS POPPED ALL
-    if line.startswith('IF'):
-        prog.pop(prog.index(line))
-# for line in prog:
-#     if line.startswith('GOTO'):
-#         prog.pop(prog.index(line))
-# for line in prog:
-#     if line.startswith('#'):
-#         prog.pop(prog.index(line))
-print(prog)
+    print(line)
 
-
-
-#     if line.startswith('%'):
-#         line = ''
-
-#     if line.startswith('O'):
-#         line = ''
-
-#     if line.startswith('('):
-#         line = ''
-
-#     if line.startswith('IF'):
-#         line = ''
-
-#     if line.startswith('#'):
-#         line = ''
-
-#     if line.startswith('G#'):
-#         line = ''
-
-#     if line.startswith('G68'):
-#         line = ''
-
-#     if line.startswith('G69'):
-#         line = ''
-
-#     if line.startswith('G50'):
-#         line = ''
-
-#     if line.startswith('G51'):
-#         line = ''
-
-#     if line.startswith('G17'):
-#         line = ''
-
-#     if line.startswith('GOTO'):
-#         line = ''
-
-#     if line.startswith('N90'):
-#         line = ''
-
-#     if line.startswith('N91'):
-#         line = ''
-
-#     if line.startswith('N99'):
-#         line = ''
-
-#     if line.startswith('G0 Z#28 (RETURN'):
-#         line = ''
-
-#     if line.find('#26'):
-#         line = line.replace('#26', '.1')
-#         line = line.replace('F#8', '')
-
-#     if line.find('F#9'):
-#         line = line.replace('F#9', '')
-
-#     line = line.strip()
-
+# Save program to a file
+with open('/Users/dennybob/Documents/GitHub/utility/dev/engrave macro/simCode.nc', 'w') as file:
+    for line in prog:
+        file.write(line + '\n')
